524f38e04e9387543f735723abe3baf20490b8c1
César Ferreira <cesar.ferreira.tn@gmail.com>
2021-05-07 23:30:41 -0300

    initial project

---
+++ virtual tourist/virtual tourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  virtual tourist
+//
+//  Created by César Ferreira on 07/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
